155317a25ad25eb7c37649efec58e58fc7be246d
Atlessc <107712050+Atlessc@users.noreply.github.com>
2023-04-04 15:59:03 -0700

    finished the process of the styling, and the API post and get scripts work, the issue was the ID for the area for the image to be displayed had an extra letter in it. now fixed. will finish it soon

--- main.js
+++ main.js
@@ -2,23 +2,44 @@ import './style.css';
 
 const form = document.querySelector('form');
 
+// 1. Listen for the form to be submitted and prevent it from being sent
 form.addEventListener('submit', async (e) => {
-    e.preventDefault();
+  e.preventDefault();
+    showSpinner();
 
-    const data = new FormData(form);
+  // 2. Take the text the user entered into the form field
+  const data = new FormData(form);
 
-    const response = await fetch('http://localhost:8080/dream', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        prompt: data.get('prompt'),
-      }),
-    });
+  // 3. Send that text to our API server
+  const response = await fetch('http://localhost:8080/dream', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt'),
+    }),
+  });
 
-    const { image } = await response.json();
+  // 4. Convert the response to JSON
+  const { image } = await response.json();
 
-    const result = document.querySelector('#result');
-    result.innerHTML = `<img src="${image}" width="512" />`;
+  // 5. Display the image on the page the area marked #result with the src being ${image}
+  const result = document.querySelector('#result');
+  result.innerHTML = `<img src="${image}" width="100%"/>`;
+  hideSpinner();
 });
+
+function showSpinner() {
+    console.log('Spinner');
+    const button = document.querySelector('button');
+    button.disabled = true;
+    button.innerHTML = `dreaming... <span class="spinner">🦄</span>`;
+}
+
+function hideSpinner() {
+    console.log('Hide Spinner');
+    const button = document.querySelector('button');
+    button.disabled = false;
+    button.innerHTML = 'Submit';
+}
