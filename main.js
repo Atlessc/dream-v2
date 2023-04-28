29c36a11f3d1c4f0871ba0729470d5e220deacf2
Atlessc <107712050+Atlessc@users.noreply.github.com>
2023-04-28 14:57:08 -0700

    final push for now

--- main.js
+++ main.js
@@ -27,8 +27,9 @@ form.addEventListener('submit', async (e) => {
   // 5. Display the image on the page the area marked #result with the src being ${image}
   const result = document.querySelector('#result');
   result.innerHTML = `<img src="${image}" width="100%"/>`;
+
+  // 6. Hide the spinner
   hideSpinner();
-  downloadBtn();
 });
 
 function showSpinner() {
@@ -43,45 +44,4 @@ function hideSpinner() {
     const button = document.querySelector('button');
     button.disabled = false;
     button.innerHTML = 'Submit';
-}
-function fetchFile(url) {
-  fetch(url).then(res => res.blob()).then(file => {
-      let tempUrl = URL.createObjectURL(file);
-      const aTag = document.createElement("a");
-      aTag.href = tempUrl;
-      aTag.download = url.replace(/^.*[\\\/]/, '');
-      document.body.appendChild(aTag);
-      aTag.click();
-      downloadBtn.innerText = "Download File";
-      URL.revokeObjectURL(tempUrl);
-      aTag.remove();
-  }).catch(() => {
-      alert("Failed to download file!");
-      downloadBtn.innerText = "Download File";
-  });
-}
-
-downloadBtn.addEventListener("click", e => {
-  e.preventDefault();
-  downloadBtn.innerText = "Downloading file...";
-  fetchFile(image.value);
-});
-// create a button to download the image and the button will be disabled until the image is loaded and load the link from the image const
-function downloadBtn() {
-    const downloadBtn = document.createElement('button');
-    downloadBtn.innerHTML = 'Download';
-    downloadBtn.addEventListener('click', () => {
-        // create an if statement to check if theres already a download button
-        if (document.querySelector('#download')) {
-          document.querySelector('#download').remove();
-      }
-        const image = document.querySelector('img');
-        const link = document.createElement('a');
-        link.setAttribute('href', image.src);
-        link.setAttribute('download', 'dream.png');
-        link.setAttribute('target', '_blank')
-        link.click();
-
-    });
-    document.body.appendChild(downloadBtn);
 }
